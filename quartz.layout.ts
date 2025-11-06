26269a04dc8e5f8df6b163c44e338c4be7f81199
Sergei S. <hello@sergeisokolov.com>
2025-11-06 21:56:54 +0300

    config tweaks

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Telegram: "https://t.me/sp404community",
+      "сайт": "https://sp404.ru/",
     },
   }),
 }
